4990c99ab717863dd8d259311159217f4dbbcab7
Muhammad Afzal Khan <mafzalkhan1997@gmail.com>
2019-02-11 21:17:58 +0500

    exploring node.js

--- app.js
+++ app.js
@@ -8,22 +8,22 @@ const _ = require("lodash");
 var filteredArray = _.uniq(["Afzal", 1, "Afzal", 1, 2, 3]);
 console.log(filteredArray);
 
-// console.log(_.isString(true));
-// console.log(_.isString("Muhammad Afzal"));
+console.log(_.isString(true));
+console.log(_.isString("Muhammad Afzal"));
 
-// var res = notes.addNote();
-// console.log(res);
+var res = notes.addNote();
+console.log(res);
 
-// console.log("Result:", notes.addition(2, 6));
+console.log("Result:", notes.addition(2, 6));
 
-// var user = os.userInfo();
+var user = os.userInfo();
 
-// fs.appendFile(
-//   "greetings.txt",
-//   `Hello ${user.username}, You are of ${notes.age} !`,
-//   error => {
-//     if (error) {
-//       console.log("unable to write file");
-//     }
-//   }
-// );
+fs.appendFile(
+  "greetings.txt",
+  `Hello ${user.username}, You are of ${notes.age} !`,
+  error => {
+    if (error) {
+      console.log("unable to write file");
+    }
+  }
+);
