6e25da7f6afb1bd5fe0ef3c3ead872200cc3d4ee
Muhammad Afzal Khan <mafzalkhan1997@gmail.com>
2019-05-04 23:44:01 +0500

    update

--- app.js
+++ app.js
@@ -5,25 +5,35 @@ const os = require("os");
 const notes = require("./notes.js");
 const _ = require("lodash");
 
-var filteredArray = _.uniq(["Afzal", 1, "Afzal", 1, 2, 3]);
-console.log(filteredArray);
+console.log(process.argv);
+let command = process.argv[2];
+if (command === "add") {
+  console.log("Adding new Note");
+} else if (command === "list") {
+  console.log("Listing all Notes");
+} else {
+  console.log("Command not Recognized");
+}
 
-console.log(_.isString(true));
-console.log(_.isString("Muhammad Afzal"));
+// var filteredArray = _.uniq(["Afzal", 1, "Afzal", 1, 2, 3]);
+// console.log(filteredArray);
 
-var res = notes.addNote();
-console.log(res);
+// console.log(_.isString(true));
+// console.log(_.isString("Muhammad Afzal"));
 
-console.log("Result:", notes.addition(2, 6));
+// var res = notes.addNote();
+// console.log(res);
 
-var user = os.userInfo();
+// console.log("Result:", notes.addition(2, 6));
 
-fs.appendFile(
-  "greetings.txt",
-  `Hello ${user.username}, You are of ${notes.age} !`,
-  error => {
-    if (error) {
-      console.log("unable to write file");
-    }
-  }
-);
+// var user = os.userInfo();
+
+// fs.appendFile(
+//   "greetings.txt",
+//   `Hello ${user.username}, You are of ${notes.age} !`,
+//   error => {
+//     if (error) {
+//       console.log("unable to write file");
+//     }
+//   }
+// );
